ebd110c5191daa3e8cfb3c0d30174f9fe07ffc3c
kc-allan <allancheruiyot003@gmail.com>
2025-01-21 02:11:11 +0300

    Initial commit

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS  users (
+id int NOT NULL AUTO_INCREMENT,
+email varchar(255) NOT NULL UNIQUE,
+name varchar(255),
+PRIMARY KEY (id)
+);
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+This is a simple exercise to test your understanding of Redis
+"""
+import redis
+import uuid
+
+
+class Cache:
+    """
+    A simple cache class that stores data in Redis
+    """
+    _redis = redis.Redis()
+
+    def __init__(self):
+        """
+        Initialize the cache
+        """
+        self._redis.flushdb()
+
+    def store(self, data: str | bytes | int | float) -> str:
+        """
+        Store the data in the cache and return the ID
+        :param data: The data to store
+        :return: The ID of the stored data
+        """
+        id = str(uuid.uuid4())
+        self._redis.sadd(id, data)
+        return id
