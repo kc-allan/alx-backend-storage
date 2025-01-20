f0cf1bda2bcf9983a4d98e0164c7c8f75515048a
kc-allan <allancheruiyot003@gmail.com>
2025-01-21 02:21:20 +0300

    Fixed redis initial connection issue

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,21 +4,22 @@ This is a simple exercise to test your understanding of Redis
 """
 import redis
 import uuid
+from typing import Union
 
 
 class Cache:
     """
     A simple cache class that stores data in Redis
     """
-    _redis = redis.Redis()
 
     def __init__(self):
         """
         Initialize the cache
         """
+        self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: str | bytes | int | float) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store the data in the cache and return the ID
         :param data: The data to store
